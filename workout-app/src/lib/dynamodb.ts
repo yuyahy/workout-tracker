13c46fc44d1eb54ed3d843aad8ad84c0f965f244
yuyahy <xvto.ya@gmail.com>
2025-11-16 08:02:24 +0900

    ステップ 4-2: DynamoDB クライアントのセットアップ　まで

---
+++ workout-app/src/lib/dynamodb.ts
@@ -0,0 +1,13 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb"
+
+const client = new DynamoDBClient({
+    region: "local",
+    endpoint: "http://localhost:8000",
+    credentials: {
+        accessKeyId: "dummy",
+        secretAccessKey: "dummy",
+    },
+})
+
+export const dynamodb = DynamoDBDocumentClient.from(client)
